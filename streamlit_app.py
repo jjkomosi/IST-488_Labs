54395881d21a095160343d399d1919ffb91e7c20
jjkomosi <jjkomosi@syr.edu>
2026-02-04 19:31:38 +0000

    navpage

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,11 @@ import streamlit as st
 
 # Define the pages
 lab1_page = st.Page("pages/Lab1.py", title="Lab 1", icon="📄")
-lab2_page = st.Page("pages/Lab2.py", title="Lab 2", icon="📝", default=True)
+lab2_page = st.Page("pages/Lab2.py", title="Lab 2", icon="📝")
+lab3_page = st.Page("pages/Lab3.py", title="Lab 3", icon="📝", default=True)
 
 # Create navigation with Lab2 as default
-nav = st.navigation([lab1_page, lab2_page])
+nav = st.navigation([lab1_page, lab2_page, lab3_page])
 
 # Run the selected page
 nav.run()
