3c9eb6fd9dafb93fdd50e38a35d8a6192ebe67d4
jjkomosi <jjkomosi@syr.edu>
2026-02-04 19:27:06 +0000

    lab3

---
+++ pages/Lab3.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from openai import OpenAI
+
+# title and description
+st.title("Jonah's Lab 3: Q/A Chatbot")
+
+openAI_model = st.sidebar.selectbox("Choose a model:",
+                                    ("mini", "regular"))
+if openAI_model == "mini":
+    model_to_use = "gpt-4o-mini"
+else:
+    model_to_use = "gpt-4o"
+
+if 'client' not in st.session_state:
+    api_key = st.secrets["OPENAI_API_KEY"]
+    st.session_state.client = OpenAI(api_key=api_key)
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = \
+    [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    chat_msg = st.chat_message(msg["role"])
+    chat_msg.write(msg["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    client = st.session_state.client
+    stream = client.chat.completions.create(
+        model=model_to_use,
+        messages = st.session_state.messages,
+        stream=True)
+    
+    with st.chat_message("assistant"):
+        response = st.write_stream(stream)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    
