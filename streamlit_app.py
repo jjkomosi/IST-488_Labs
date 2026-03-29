c1f46e1972083f6f3dab8519f3bb55417a8fb5d2
jonahkomo <jonahkomo12@gmail.com>
2026-03-29 14:12:29 -0400

    lab6again

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,11 @@ lab1_page = st.Page("pages/Lab1.py", title="Lab 1", icon="📄")
 lab2_page = st.Page("pages/Lab2.py", title="Lab 2", icon="📝")
 lab3_page = st.Page("pages/Lab3.py", title="Lab 3", icon="📝")
 lab4_page = st.Page("pages/Lab4.py", title="Lab 4", icon="📝")
-lab5_page = st.Page("pages/Lab5.py", title="Lab 5", icon="📝", default=True)
+lab5_page = st.Page("pages/Lab5.py", title="Lab 5", icon="📝")
+lab6_page = st.Page("pages/Lab6.py", title="Lab 6", icon="📝", default=True)
 
 # Create navigation with Lab2 as default
-nav = st.navigation([lab1_page, lab2_page, lab3_page, lab4_page, lab5_page])
+nav = st.navigation([lab1_page, lab2_page, lab3_page, lab4_page, lab5_page, lab6_page])
 
 # Run the selected page
 nav.run()
